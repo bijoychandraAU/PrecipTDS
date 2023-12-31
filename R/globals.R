5a5242d4952bbf086bede29839606f38051dc9f8
bijoychandra, Ph.D <54862183+bijoychandraAU@users.noreply.github.com>
2023-12-30 20:08:06 -0500

    Create globals.R
    This is for making global variables

---
+++ R/globals.R
@@ -0,0 +1,3 @@
+utils::globalVariables(c("Depth", "Precip_mm", "Rain_hour_mm", "Storm",
+                         "datetime", "dur", "event", "event1", "event_no",
+                         "strmlen", "time2"))
