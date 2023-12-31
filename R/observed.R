bb93e66e7a7434190bf901a65367df19c7970558
bijoychandra, Ph.D <54862183+bijoychandraAU@users.noreply.github.com>
2023-12-30 20:07:33 -0500

    Data
    Both observed and simulated data were updated

---
+++ R/observed.R
@@ -0,0 +1,12 @@
+#' 15-min Observed Precipitation data
+#'
+#' Precipitation data for a station recorded at 15-min intervals from 1981-2000.
+#'
+#' @format
+#' A data frame with 10,086 rows and 2 columns:
+#' \describe{
+#'   \item{DT}{Date Time in YYYY-MM-DD hh:mm:ss}
+#'   \item{PRECIP}{Observed precipitation data recorded at 15-min intervals}
+#' }
+#' @source https://doi.org/10.1038/s41597-022-01304-7
+"observed"
