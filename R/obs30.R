003a9c02b5ab3d3ec0c5c2207e7ec340d73530b1
bijoychandra, Ph.D <54862183+bijoychandraAU@users.noreply.github.com>
2023-12-31 19:00:10 -0500

    documented for observed data

---
+++ R/obs30.R
@@ -0,0 +1,12 @@
+#' 30-min Observed Precipitation data
+#'
+#' Precipitation data for a station recorded at 30-min intervals from 1981-2000.
+#'
+#' @format
+#' A data frame with 8,881 rows and 2 columns:
+#' \describe{
+#'   \item{DT}{Date Time in YYYY-MM-DD hh:mm:ss}
+#'   \item{PRECIP}{Observed precipitation data recorded at 30-min intervals}
+#' }
+#' @source https://doi.org/10.1038/s41597-022-01304-7
+"obs30"
