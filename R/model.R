33f9de033cd95dfff2ccb1d5b40813491261404e
bijoychandra, Ph.D <54862183+bijoychandraAU@users.noreply.github.com>
2023-12-31 19:49:04 -0500

    Create model.R

---
+++ R/model.R
@@ -0,0 +1,12 @@
+#' Hourly simulated Precipitation data
+#'
+#' Precipitation data for a station recorded at 1-hour intervals for 2030.
+#'
+#' @format
+#' A data frame with 288 rows and 2 columns:
+#' \describe{
+#'   \item{DateTime}{Date Time in YYYY-MM-DD hh:mm:ss at 1-hour intervals.}
+#'   \item{PRECIP}{Simulated precipitation data.}
+#'   }
+#' @source https://doi.org/10.1038/s41597-022-01304-7
+"model"
